9b913f4d3c1ad242e140ae5de0aee93ef06d60f5
Joe Rocca <jprocca813@gmail.com>
2017-07-21 00:43:05 -0400

    Clean up

--- tvOSTimer/tvOSTimer/ZScaleVerticalAnimationController.swift
+++ tvOSTimer/tvOSTimer/ZScaleVerticalAnimationController.swift
@@ -8,25 +8,23 @@
 
 import UIKit
 
-class ZScaleVerticalAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
-    
+class ZScaleVerticalAnimationController: NSObject {
+    //MARK: Properties
     var reverse: Bool = false
-    
+}
+
+extension ZScaleVerticalAnimationController: UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 0.5
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        
         let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
         let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
         
         let finalFrameForVC = transitionContext.finalFrame(for: toViewController)
-        
         let containerView = transitionContext.containerView
-        
         let bounds = UIScreen.main.bounds
-        
         
         if self.reverse {
             toViewController.view.transform = CGAffineTransform(scaleX: 0.90, y: 0.90)
@@ -38,26 +36,19 @@ class ZScaleVerticalAnimationController: NSObject, UIViewControllerAnimatedTrans
         }
         
         UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, usingSpringWithDamping: 0.90, initialSpringVelocity: 0.0, options: UIViewAnimationOptions(), animations: {
-            
             if self.reverse {
-                //                    fromViewController.view.alpha = 0.0
                 fromViewController.view.frame = finalFrameForVC.offsetBy(dx: 0, dy: bounds.size.height)
                 toViewController.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
                 toViewController.view.frame = finalFrameForVC
-                
             } else {
                 fromViewController.view.alpha = 0.8
                 fromViewController.view.transform = CGAffineTransform(scaleX: 0.90, y: 0.90)
                 toViewController.view.frame = finalFrameForVC
             }
-            
-            
-            }, completion: { finished in
-                
-                transitionContext.completeTransition(true)
-                fromViewController.view.alpha = 1.0
-                fromViewController.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: { finished in
+            transitionContext.completeTransition(true)
+            fromViewController.view.alpha = 1.0
+            fromViewController.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
         })
-        
     }
 }
